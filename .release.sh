aaca45a2ccd457bb00e5b362a0aee8f2f651c70b
l.aminov <l.aminov@tinkoff.ru>
2018-10-05 12:36:37 +0500

    Fix build

--- .release.sh
+++ .release.sh
@@ -2,10 +2,9 @@
 
 set -x
 
-GOOS=linux
 VERSION=$(cat main.go | grep "Version =" | awk -F\" '{print $2}')
 FILENAME="alertmanager-devops-toolkit-$VERSION.linux-amd64"
 
-go build -o "$FILENAME"
+GOOS=linux go build -o "$FILENAME"
 dope nexus upload -d prometheus "$FILENAME"
 rm -f "$FILENAME"
