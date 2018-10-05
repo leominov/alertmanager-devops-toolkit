6590e8c2c96578b0cd04f13c97e73798dd07bb99
l.aminov <l.aminov@tinkoff.ru>
2018-10-05 11:41:10 +0500

    Add release script

---
+++ .release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+VERSION=$(cat main.go | grep "Version =" | awk -F\" '{print $2}')
+FILENAME="alertmanager-devops-toolkit-$VERSION.linux-amd64"
+GOOS=linux go build -o "$FILENAME"
+dope nexus upload -d prometheus "$FILENAME"
