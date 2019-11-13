a1a96ec9e8834894ac224986ccb4bac48a9e6816
l.aminov <l.aminov@tinkoff.ru>
2019-11-13 09:03:24 +0500

    Bump version

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ var (
 	TestDir            = flag.String("test-dir", "tests", "Directory with config tests")
 	ShowVersion        = flag.Bool("version", false, "Prints version and exit")
 
-	Version = "1.3.1"
+	Version = "1.3.2"
 )
 
 func templateVars(dir string) (map[string]interface{}, error) {
